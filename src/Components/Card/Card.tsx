c818df2c688cc07938c179f2f028ef81c746b5f8
redmejia <reynaldoemejia@gmail.com>
2024-06-27 22:13:41 -0700

    menu testing data

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,43 +1,48 @@
-import { StyleSheet, Text, View, TouchableOpacity, ImageBackground } from "react-native"
+import { StyleSheet, Text, View, TouchableOpacity, ImageBackground, ImageSourcePropType } from "react-native"
 import { colors, fontSize, spacing } from "../../theme";
 import { CustomButton } from "../CustomButton";
 
 
 interface Props {
-    // increment and decrement function will be require 
+    product: string;
+    price: number;
+    counter: number;
+    image: ImageSourcePropType
+
     increment?: () => void;
     decrement?: () => void;
 }
 
 export const Card = (props: Props) => {
 
-    const { decrement, increment } = props
+    const { product, price, counter, image, decrement, increment } = props
 
     return (
         <TouchableOpacity activeOpacity={0.9} style={styles.container}>
-            <ImageBackground source={require("../../assets/banana.png")} resizeMode="cover" style={styles.image}>
+            <ImageBackground source={image} resizeMode="cover" style={styles.image}>
                 <View style={styles.orderCounterContainer}>
-                    <View style={styles.orderCounter}>
-                        <Text style={[styles.text, { fontSize: fontSize.lg, fontWeight: '800' }]} > 12 </Text>
+                    <View style={{ ...styles.orderCounter, backgroundColor: counter > 0 ? colors.red : 'transparent' }}>
+                        <Text style={[styles.text, { fontSize: fontSize.lg, fontWeight: '800' }]} > {counter} </Text>
                     </View>
+
                 </View>
                 <View style={{ backgroundColor: colors.transparent_dark, }}>
-                    <Text style={{...styles.text, fontWeight:'400'}}>Banana</Text>
-                    <Text style={[styles.text, { fontSize: fontSize.xl, fontWeight: '500' }]} >$12</Text>
+                    <Text style={{ ...styles.text, fontWeight: '400' }}>{product}</Text>
+                    <Text style={[styles.text, { fontSize: fontSize.xl, fontWeight: '500' }]} >$ {price}</Text>
                 </View>
                 <View style={{ flexDirection: 'row', justifyContent: 'center', padding: 20, }}>
                     <CustomButton
                         onPress={decrement}
                         btnText="-"
                         btnStyle={{ width: '50%' }}
-                        btnTextStyle={{ color: colors.white, fontWeight:'900' }}
+                        btnTextStyle={{ color: colors.white, fontWeight: '900' }}
                     />
                     <View style={{ padding: 4 }} />
                     <CustomButton
                         onPress={increment}
                         btnText="+"
                         btnStyle={{ backgroundColor: colors.dark_blue, width: '50%' }}
-                        btnTextStyle={{ color: colors.white, fontWeight:'900' }}
+                        btnTextStyle={{ color: colors.white, fontWeight: '900' }}
                     />
                 </View>
             </ImageBackground>
@@ -73,6 +78,8 @@ const styles = StyleSheet.create({
         borderRadius: 50
     },
     image: {
+        borderRadius: 10,
+        overflow: 'hidden',
         justifyContent: 'center',
     },
     text: {
